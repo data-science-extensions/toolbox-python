37d40aabbee8f5e888c95de0e7dc29c461efb17b
Chris Mahoney <44449504+chrimaho@users.noreply.github.com>
2025-04-25 10:56:19 +1000

    Enhance `class_property` decorator with additional class-level properties, and add unit tests with 100% coverage

--- src/tests/test_classes.py
+++ src/tests/test_classes.py
@@ -67,11 +67,34 @@ class MyClass:
     _class_value: str = "original"
 
     def __init__(self, instance_value: str = "instance") -> None:
-        self._instance_value = instance_value
+        self._instance_value: str = instance_value
 
     @class_property
     def class_value(cls) -> str:
         return cls._class_value
+
+    # @class_value.getter
+    # def class_value_getter(cls) -> str:
+    #     return cls._class_value
+
+    @class_property
+    def class_value_modified(cls) -> str:
+        return cls._class_value + " modified"
+
+    @class_property()
+    def class_value_with_parens(cls) -> str:
+        return cls._class_value + " with parens"
+
+    @class_property(doc="This is a class property with a docstring.")
+    def class_value_with_doc(cls) -> str:
+        """This is a class property with a docstring."""
+        return cls._class_value + " with doc"
+
+    @class_property
+    @classmethod
+    def class_value_method(cls) -> str:
+        """This is a class method."""
+        return cls._class_value + " with method"
 
     @property
     def instance_value(self) -> str:
@@ -98,15 +121,36 @@ class TestClassProperty(TestCase):
         assert new_class.class_value == "original"
         assert new_class.instance_value == "instance"
 
+    # def test_class_property_getter(self) -> None:
+    #     assert MyClass.class_value_getter == MyClass.class_value
+
     def test_class_property_modified_properties(self) -> None:
+
+        # Test the instance property for an instance of MyClass
         new_class = MyClass("instance value")
         assert new_class.instance_value == "instance value"
+
+        # Test the modified value of an instance property for the instantiated class
         new_class._instance_value = "new instance value"
         assert new_class.instance_value == "new instance value"
-        new_class._class_value = "new class value"
-        assert new_class.class_value == "new class value"
 
-    def test_class_property_missing(self) -> None:
+        # Test the class property for an instance of MyClass
+        # This should return the original class value, not the modified one
+        new_class._class_value = "new class value"
+        assert new_class.class_value == "original"
+
+    def test_class_property_with_parens(self) -> None:
+        assert MyClass.class_value_with_parens == "original with parens"
+        MyClass._class_value = "modified"
+        assert MyClass.class_value_with_parens == "modified with parens"
+
+    def test_class_property_with_doc(self) -> None:
+        assert MyClass.class_value_with_doc == "original with doc"
+        MyClass.class_value_with_doc.__doc__ == (
+            "This is a class property with a docstring."
+        )
+
+    def test_class_property_errors(self) -> None:
         with raises(AttributeError):
             MyClass().missing_property
         with raises(AttributeError):
@@ -115,3 +159,33 @@ class TestClassProperty(TestCase):
             MyClass().missing_class_property
         with raises(AttributeError):
             MyClass.missing_class_property
+
+    def test_class_property_setter(self) -> None:
+
+        with raises(NotImplementedError):
+
+            class MyClassWithSetter:
+                _class_value: str = "original"
+
+                @class_property
+                def class_value(cls) -> str:
+                    return cls._class_value
+
+                @class_value.setter
+                def class_value(cls, value: str) -> None:
+                    cls._class_value = value
+
+    def test_class_property_deleter(self) -> None:
+
+        with raises(NotImplementedError):
+
+            class MyClassWithDeleter:
+                _class_value: str = "original"
+
+                @class_property
+                def class_value(cls) -> str:
+                    return cls._class_value
+
+                @class_value.deleter
+                def class_value(cls) -> None:
+                    del cls._class_value
--- src/toolbox_python/classes.py
+++ src/toolbox_python/classes.py
@@ -39,6 +39,7 @@
 
 
 # ## Python StdLib Imports ----
+from functools import wraps
 from typing import Any
 
 # ## Local First Party Imports ----
@@ -118,80 +119,185 @@ def get_full_class_name(obj: Any) -> str:
     return module + "." + obj.__class__.__name__
 
 
-class class_property:
+# TODO: This can still be made to work for setters by implementing an accompanying metaclass that supports it.
+class class_property(property):
     """
     !!! note "Summary"
-        A decorator similar to `@property` but works on class methods instead of instance methods.
+        Similar to `property`, but allows class-level properties. That is, a property whose getter is like a `classmethod`.
 
     ???+ abstract "Details"
-        Allows defining class-level properties that are computed when accessed rather than stored.
+        The wrapped method may explicitly use the `classmethod` decorator (which must become before this decorator), or the `classmethod` may be omitted (it is implicit through use of this decorator).
+
+    Params:
+        fget (callable):
+            The function that computes the value of this property (in particular, the function when this is used as a decorator) a la `property`.
+
+        doc (str, optional):
+            The docstring for the property--by default inherited from the getter function.
 
     ???+ example "Examples"
 
-        ```{.py .python linenums="1" title="Set up"}
-        >>> from toolbox_python.classes import class_property
-        ```
+        Normal usage is as a decorator:
 
-        ```{.py .python linenums="1" title="Example 1: Create a class property"}
-        >>> class MyClass:
-        ...     _class_value: str = "original"
+        ```{.py .python linenums="1" title="Example 1: Normal usage"}
+        >>> class Foo:
+        ...     _bar_internal = 1
         ...
         ...     @class_property
-        ...     def class_value(cls) -> str:
-        ...         return cls._class_value
-        ...
-        >>> print(MyClass.class_value)
+        ...     def bar(cls):
+        ...         return cls._bar_internal + 1
+        >>>
+        >>> print(f"Class attribute: `{Foo.bar}`")
+        >>>
+        >>> foo_instance = Foo()
+        >>> print(f"Instantiated class: `{foo_instance.bar}`")
+        >>>
+        >>> foo_instance._bar_internal = 2
+        >>> print(f"Modified instance attribute: `{foo_instance.bar}`")  # Ignores instance attributes
         ```
         <div class="result" markdown>
         ```{.sh .shell title="Terminal"}
-        original
+        Class attribute: `2`
+        Instantiated class: `2`
+        Modified instance attribute: `2`
         ```
-        !!! success "Conclusion: Successful class property creation."
+        Note that in the third `print()` statement, the instance attribute `_bar_internal` is ignored. This is because `class_property` is designed to be used as a class-level property, not an instance-level property. See the Notes section for more details.
+        !!! success "Conclusion: Successful usage."
         </div>
 
-        ```{.py .python linenums="1" title="Example 2: Create a class property with instance override"}
-        >>> class MyClass:
-        ...     _class_value: str = "original"
-        ...
-        ...     def __init__(self, instance_value: str = "instance") -> None:
-        ...         self._instance_value = instance_value
+        As previously noted, a `class_property` is limited to implementing read-only attributes:
+
+        ```{.py .python linenums="1" title="Example 2: Read-only attributes"}
+        >>> class Foo:
+        ...     _bar_internal = 1
         ...
         ...     @class_property
-        ...     def class_value(cls) -> str:
-        ...         return cls._class_value
+        ...     def bar(cls):
+        ...         return cls._bar_internal
         ...
-        ...     @property
-        ...     def instance_value(self) -> str:
-        ...         return self._instance_value
-        ...
-        ...
-        >>> my_instance = MyClass()
-        >>> print(my_instance.class_value)
-        >>> print(my_instance.instance_value)
+        ...     @bar.setter
+        ...     def bar(cls, value):
+        ...         cls._bar_internal = value
         ```
         <div class="result" markdown>
         ```{.sh .shell title="Terminal"}
-        original
-        instance
+        Traceback (most recent call last):
+        ...
+        NotImplementedError: class_property can only be read-only; use a metaclass to implement modifiable class-level properties
         ```
-        !!! success "Conclusion: Successful class property with instance override."
+        !!! failure "Conclusion: Failed to set a class property."
         </div>
+
+    ???+ info "Notes"
+        - `@class_property` only works for *read-only* properties. It does not currently allow writeable/deletable properties, due to subtleties of how Python descriptors work. In order to implement such properties on a class, a metaclass for that class must be implemented.
+        - `@class_property` is not a drop-in replacement for `property`. It is designed to be used as a class-level property, not an instance-level property. If you need to use it as an instance-level property, you will need to use the `@property` decorator instead.
+        - `@class_property` is defined at class scope, not instance scope. This means that it is not bound to the instance of the class, but rather to the class itself; hence the name `class_property`. This means that it is designed to be used as a class-level property and is accessed through the class itself, not an instance-level property which is accessed through the instance of a class. If it is necessary to access the instance-level property, you will need to use the instance itself (eg. `instantiated_class_name._internal_attribute`) or create an instance-level property using the `@property` decorator.
+
+    ???+ success "Credit"
+        This `@class_property` object is heavily inspired by the [`astropy`](https://www.astropy.org/) library. All credit goes to them. See: [details](https://github.com/astropy/astropy/blob/e4993fffb54e19b04bc4e9af084984650bc0a46f/astropy/utils/decorators.py#L551-L722).
     """
 
-    def __init__(self, method: Any) -> None:
-        self.method: Any = method
-        self.name: str = method.__name__
-        self.__doc__ = method.__doc__
+    def __new__(cls, fget=None, doc=None):
 
-    def __get__(self, instance: Any, cls=None) -> Any:
+        if fget is None:
 
-        # Check for instance-level override
-        if instance is not None:
+            # Being used as a decorator-return a wrapper that implements decorator syntax
+            def wrapper(func):
+                return cls(func, doc=doc)
 
-            # Look for _attribute_name pattern
-            private_name: str = f"_{self.name}"
-            if hasattr(instance, private_name):
-                return getattr(instance, private_name)
+            return wrapper
 
-        # Fall back to class method
-        return self.method(cls)
+        return super().__new__(cls)
+
+    def __init__(self, fget, doc=None):
+
+        fget = self._wrap_fget(fget)
+        super().__init__(fget=fget, doc=doc)
+
+        # There is a bug in Python where `self.__doc__` doesn't get set properly on instances of property subclasses if the `doc` argument was used rather than taking the docstring from `fget`.
+        # Related Python issue: https://bugs.python.org/issue24766
+        if doc is not None:
+            self.__doc__ = doc
+
+    def __get__(self, obj, objtype) -> Any:  # type: ignore[override]
+        # The base `property.__get__` will just return self here; instead we pass `objtype` through to the original wrapped function (which takes the `class` as its sole argument). This is how we obtain the `class` attribute and not the `instance` attribute, and the key difference between `@property` and `@class_property`. This is also the same as `classmethod.__get__()`, but we need to pass `objtype` rather than `obj` to the wrapped function.
+        val: Any = self.fget.__wrapped__(objtype)  # type: ignore[union-attr]
+        return val
+
+    @staticmethod
+    def _wrap_fget(orig_fget):
+        if isinstance(orig_fget, classmethod):
+            orig_fget = orig_fget.__func__
+
+        # Using standard `functools.wraps` for simplicity.
+        @wraps(orig_fget)  # pragma: no cover
+        def fget(obj):
+            return orig_fget(obj.__class__)
+
+        return fget
+
+    # def getter(self, fget) -> property:
+    #     return super().getter(self._wrap_fget(fget))
+
+    def setter(self, fset) -> Any:
+        raise NotImplementedError(
+            "class_property can only be read-only; use a metaclass to implement modifiable class-level properties"
+        )
+
+    def deleter(self, fdel) -> Any:
+        raise NotImplementedError(
+            "class_property can only be read-only; use a metaclass to implement modifiable class-level properties"
+        )
+
+
+# def cached_class_property(func) -> property:
+#     """
+#     !!! note "Summary"
+#         This function is designed to cache the result of a class property, so that it is only computed once, and then stored for future reference.
+
+#     ???+ abstract "Details"
+#         This is useful for properties that are computationally expensive, but are not expected to change over the lifetime of the object.
+
+#     Params:
+#         func (Callable):
+#             The function to be cached.
+
+#     Returns:
+#         (property):
+#             The cached property.
+
+#     ???+ example "Examples"
+
+#         ```{.py .python linenums="1" title="Set up"}
+#         >>> from toolbox_python.classes import cached_class_property
+#         ```
+
+#         ```{.py .python linenums="1" title="Example 1: Create a cached class property"}
+#         >>> class TestClass:
+#         ...     @cached_class_property
+#         ...     def expensive_property(cls):
+#         ...         print("Computing expensive property...")
+#         ...         return 42
+#         ...
+#         >>> print(TestClass.expensive_property)
+#         >>> print(TestClass.expensive_property)
+#         ```
+#         <div class="result" markdown>
+#         ```{.sh .shell title="Terminal"}
+#         Computing expensive property...
+#         42
+#         42
+#         ```
+#         !!! success "Conclusion: Successful property caching."
+#         </div>
+#     """
+#     attr_name = "_cached_" + func.__name__
+
+#     @property
+#     @wraps(func)
+#     def wrapper(self):
+#         if not hasattr(self, attr_name):
+#             setattr(self, attr_name, func(self))
+#         return getattr(self, attr_name)
+
+#     return wrapper
